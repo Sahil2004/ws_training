cbcc38569494775f1ad0dfe3b78e7461ba6073ea
Sahil Garg <me.sahil.gg@gmail.com>
2026-05-05 11:35:53 +0530

    practical section also done with region CRUD

--- 04_module_d_2/src/mainRouter.js
+++ 04_module_d_2/src/mainRouter.js
@@ -1,9 +1,62 @@
 import { Router } from "express";
+import { body, validationResult } from "express-validator";
+import { prisma } from "./db.js";
 
-const router = Router()
+const router = Router();
 
-router.get("/", (req, res) => {
-    return res.render("main")
-})
+router.get("/", async (req, res) => {
+  const practicalInfo = await prisma.infoItems.findMany({});
+  const regionsInfo = await prisma.regions.findMany({
+    include: {
+      attractions: true,
+    },
+  });
+  return res.render("main", {
+    infoItems: practicalInfo,
+    regionsInfo: regionsInfo,
+  });
+});
 
-export { router as mainRouter }
+router.post(
+  "/submission",
+  body("name").notEmpty(),
+  body("message").notEmpty(),
+  body("email").isEmail(),
+  async (req, res) => {
+    const result = validationResult(req);
+    if (!result.isEmpty()) {
+      return res.render("main", {
+        of: "contact",
+        success: false,
+        message: "Validation Failed!",
+      });
+    }
+    try {
+      await prisma.submissions.create({
+        data: req.body,
+      });
+      const practicalInfo = await prisma.infoItems.findMany({});
+      const regionsInfo = await prisma.regions.findMany({
+        include: {
+          attractions: true,
+        },
+      });
+      return res.render("main", {
+        of: "contact",
+        success: true,
+        message:
+          "Thanks! We've received your request. We'll reach out with tips for the region.",
+        infoItems: practicalInfo,
+        regionsInfo: regionsInfo,
+      });
+    } catch (err) {
+      return res.render("main", {
+        of: "contact",
+        success: false,
+        message: "Internal Server Error",
+      });
+    }
+  },
+);
+
+export { router as mainRouter };
